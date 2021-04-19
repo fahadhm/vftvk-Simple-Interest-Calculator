81a7ee6d8ef59c9b797e28c46f9609a0b6fc7fe2
fahadhm <82405742+fahadhm@users.noreply.github.com>
2021-04-19 16:33:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function compute()
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value.onchange=compute;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100
+var year = new Date().getFullYear()+parseInt(years);
+
+function updateRate() 
 {
-    p = document.getElementById("principal").value;
-    
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+    result = document.getElementById("result");
+    document.getElementById("result").innerHTML = (principal*rate*years/100);
+
 }
+    
+
         
